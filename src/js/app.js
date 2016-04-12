8c43b190b43ec7140b76fe558d1e404488d280c4
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2016-04-12 11:09:33 -0400

    Add src/templates/about.html file. Add changes to src/index.html (add jquery tag) and src/js/app.js (use template manager).

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,18 @@
-(function () {
+(function (jQuery) {
   this.conocerd = this.conocerd || {};
 
   var Router = this.conocerd.route.Router;
+  var TemplateManager = this.conocerd.views.TemplateManager;
   Router
     .config({ mode: 'hash' })
     .add('/', function () {
       console.log('default');
     })
     .add('/about', function () {
-      console.log('about');
+      TemplateManager.getTemplate('/about.html', function (template) {
+        jQuery('body').html(template.html);
+        console.log('about');
+      });
     })
     .add('/products/:id/edit/:value', function () {
       console.log('products', this.routeParams);
@@ -17,4 +21,4 @@
 
   // returning the user to the initial state
   Router.navigate(Router.getFragment());
-}());
+}(window.$));
