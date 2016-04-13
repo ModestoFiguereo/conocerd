3b5ac6096184e1b04bbfdbdaa75fe62475a394df
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2016-04-13 18:20:44 -0400

    Add changes to gulpfile.babel.js (enhance task structure) and package.json (add run-sequence and yargs dependencies).

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,8 +7,16 @@ import minifyjs from 'gulp-minify';
 import minifycss from 'gulp-cssmin';
 import eslint from 'gulp-eslint';
 import rimraf from 'rimraf';
+import sequence from 'run-sequence';
+import {argv as args} from 'yargs';
+import del from 'del';
 
 const TMP_DIR = '.tmp';
+const TMP_FILES = []
+  .concat(`**/${TMP_DIR}/*`)
+  .concat('**/.tmp')
+  .concat('**/*.tmp')
+  .concat('**/*.tmp.*');
 
 const SOURCE_DIR = 'src';
 const SOURCE_FILES = []
@@ -27,24 +35,21 @@ const JS_FILES = []
   .concat(`${JS_DIR}/**/*.js`);
 
 const BUILD_DIR = 'dist';
-const BUILD_FILES = `${BUILD_DIR}/**/*.*`;
+const BUILD_FILES = `${BUILD_DIR}/**/*`;
 
 const SERVER_DIR = 'server';
 const SERVER_FILES = `${SERVER_DIR}/**/*.es6`;
 
 /*
- * Clean folders genereted by running
- * other tasks.
+ * Clean tmp files and folders and build
  */
-gulp.task('clean-build', (done) => rimraf(BUILD_DIR, done));
-gulp.task('clean-src-tmp', (done) => rimraf(`${SOURCE_DIR}/${TMP_DIR}`, done));
-gulp.task('clean-server-tmp', (done) => rimraf(`${TMP_DIR}`, done));
+ gulp.task('clean', () => del(TMP_FILES.concat(BUILD_DIR)));
 
 /*
  * Ensure js files have no syntax errors and
  * complay with code standards.
  */
-gulp.task('lint', () => {
+gulp.task('lint-js', () => {
   const filesToLint = JS_FILES;
 
   return gulp.src(filesToLint)
@@ -63,13 +68,11 @@ gulp.task('concat-css', () => gulp
   .pipe(gulp.dest(`${SOURCE_DIR}/.tmp`))
 );
 
-gulp.task('concat-js', ['lint'], () => gulp
+gulp.task('concat-js', () => gulp
   .src(JS_FILES, { base: JS_DIR })
   .pipe(concat('conocerd.js'))
   .pipe(gulp.dest(`${SOURCE_DIR}/.tmp`))
 );
-
-gulp.task('concat', ['concat-css', 'concat-js'], (done) => done());
 
 /*
  * Minify conocerd.css and conocerd.js, and
@@ -91,12 +94,10 @@ gulp.task('minify-js', () => gulp
   .pipe(gulp.dest(`${SOURCE_DIR}/.tmp`))
 );
 
-gulp.task('minify', ['concat', 'minify-css', 'minify-js'], (done) => done());
-
 /*
  * Build app. Get all files that matter into `dist/` folder.
  */
-gulp.task('build', ['clean-build', 'minify'], () => gulp
+gulp.task('build', () => gulp
   .src(SOURCE_FILES)
   .pipe(gulp.dest(BUILD_DIR))
 );
@@ -104,7 +105,7 @@ gulp.task('build', ['clean-build', 'minify'], () => gulp
 /*
  * Transpile server files and get them to `.tmp/` folder.
  */
-gulp.task('compile-server', ['clean-server-tmp'], () => gulp
+gulp.task('compile-server', () => gulp
   .src(SERVER_FILES, { base: '.' })
   .pipe(babel())
   .pipe(rename((p) => { p.extname = '.js'; }))
@@ -115,7 +116,21 @@ gulp.task('compile-server', ['clean-server-tmp'], () => gulp
  * Ship build files to `.tmp/public/` folder so the server
  * can serve them.
  */
-gulp.task('ship', ['build', 'compile-server'], () => gulp
+gulp.task('ship', () => gulp
   .src(BUILD_FILES, { base: BUILD_DIR })
   .pipe(gulp.dest(`${TMP_DIR}/public`))
 );
+
+/*
+ * Run all the above task in sequence.
+ */
+gulp.task('deploy', (done) => {
+  sequence(
+    'clean',
+    'lint-js',
+    [ 'concat-css', 'concat-js' ],
+    [ 'minify-css', 'minify-js' ],
+    [ 'build', 'compile-server' ],
+    'ship',
+    done);
+})
