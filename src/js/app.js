674a0b6c848547bdaf3ea4c45c8501d61959ded1
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2016-05-03 22:58:29 -0400

    Add changes to src/js/app.js and src/js/conocerd.template-manager.js (replace callback with promise).

--- src/js/app.js
+++ src/js/app.js
@@ -2,33 +2,37 @@
   var Router = namespace.import('conocerd.route').Router;
   var TemplateManager = namespace.import('conocerd.views').TemplateManager;
   var $ = namespace.import('jQuery');
+  var views = namespace.import('conocerd.views');
+  var config = namespace.import('config');
 
   Router
     .config({ mode: 'hash' })
-    .add('/', function () {
-      console.log('default');
-    })
+    .add('/', views.HomeView)
+    .add('/home', views.HomeView)
     .add('/about', function () {
       console.log(this);
-      TemplateManager.getTemplate('/about.html', function (template) {
-        $('body').html(template.compile({
-          title: 'About',
-          body: 'We\'re conocerd.com!'
-        }));
-      });
+      TemplateManager.getTemplate('/about.html')
+        .then(function (template) {
+          $(config.views.VIEWS_SECTION).empty().append(template.compile({
+            title: 'About',
+            body: 'We\'re conocerd.com!'
+          }));
+        });
     })
     .add('/products/:id/', function () {
       console.log(this);
       var params = this.params;
-      TemplateManager.getTemplate('/product.html', function (template) {
-        $('body').html(template.compile(params));
-      });
+      TemplateManager.getTemplate('/product.html')
+        .then(function (template) {
+          $(config.views.VIEWS_SECTION).empty().append(template.compile(params));
+        });
     })
     .add('/404', function () {
       var params = this.params;
-      TemplateManager.getTemplate('/404.html', function (template) {
-        $('body').html(template.compile(params));
-      });
+      TemplateManager.getTemplate('/404.html')
+        .then(function (template) {
+          $(config.views.VIEWS_SECTION).empty().append(template.compile(params));
+        });
     })
     .otherwise(function () {
       this.navigate('404');
--- src/js/conocerd.template-manager.js
+++ src/js/conocerd.template-manager.js
@@ -2,24 +2,33 @@
   var ns = namespace('conocerd.views');
   var Template = namespace.import('conocerd.views').Template;
   var $ = namespace.import('jQuery');
+  var Promise = namespace.import('Promise');
 
   ns.TemplateManager = (function () {
     var templates = [];
 
     return {
-      getTemplate: function (url, callback) {
+      getTemplate: function (url) {
         var template = templates.find(function (x) { return x.url === url; });
 
-        if (template) {
-          callback(template);
-        } else {
-          $.get(url).done(function (source) {
-            template = new Template(url, source);
+        return new Promise(function resolver(resolve, reject) {
+          if (template) {
+            resolve(template);
+          } else {
+            $.get(url).done(function (source) {
+              template = new Template(url, source);
 
-            templates.push(template);
-            callback(template);
-          });
-        }
+              templates.push(template);
+              resolve(template);
+            })
+            .fail(function (err) {
+              reject(err);
+            });
+          }
+        })
+        .catch(function () {
+          console.log('Could not load ' + url + ' template.');
+        });
       }
     };
   }());
